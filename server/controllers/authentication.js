c142d43e354090911a32c8da5652d28d634034d5
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-10 23:20:29 -0800

    full backend authentication

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,10 +1,15 @@
-cpmst jwt = require('jwt-simple');
+const jwt = require('jwt-simple');
 const User = require('../models/user');
 const config = require('../config');
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
   return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function(req, res, next) {
+  
+  res.send({ token: tokenForUser(req.user) });
 }
 
 exports.signup = function(req, res, next) {
