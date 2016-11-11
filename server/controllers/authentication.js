1de5e4733d54094217e557fd967d110459827fe0
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-10 22:13:54 -0800

    adding loging passport strategy

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,13 +1,18 @@
+cpmst jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
 
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
-
   let email = req.body.email;
   let password = req.body.password;
 
   if (!email || !password) {
-    return res.status(422).send({ error: "You must provide email and password" });
+    res.status(422).send({ error: "You must provide email and password" });
   }
 
   User.findOne({ email: email }, function(err, existingUser) {
@@ -25,7 +30,7 @@ exports.signup = function(req, res, next) {
     user.save(function(err) {
       if(err) { return next(err); }
       
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
       
   })
