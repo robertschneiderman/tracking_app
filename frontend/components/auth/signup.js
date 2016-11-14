1611c78ab01cd0e73498d7713c4aff05f66e80c5
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-13 18:45:35 -0800

    base signup form with validation

--- frontend/components/auth/signup.js
+++ frontend/components/auth/signup.js
@@ -1,21 +1,55 @@
 import React, { Component} from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 // import Container from './/_container';
 
-class Signup extends Component {
+const validate = values => {
+  const errors = {};
 
-  constructor(props) {
-    super(props);
+  if (!values.username) {
+    errors.email = 'Required';
+  } else if (!values.password) {
+    errors.password = 'Required';    
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  } else if (values.password !== values.passwordConfirm) {
+    errors.password = 'Passwords must match';
   }
 
-  render() {
-    return(
-      <div className="">
-        Signup
-      </div>
-    )
-  }
+  return errors;
 }
 
-export default Signup;
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <label>{label}:</label>
+    <div>
+      <input className="form-control" {...input} placeholder={label} type={type}/>
+      {touched && error && <span>{error}</span>}
+    </div>
+  </div>
+);
+
+const Signup = (props) => {
+  const { handleSubmit, pristine, reset, submitting } = props;
+
+  return(
+    <form className="">
+      <fieldset className="form-group">
+        <Field name="email" component={renderField} label="Email" />
+      </fieldset>
+      <fieldset className="form-group">
+        <Field name="password" component={renderField} label="Password" type="password" />
+      </fieldset>
+      <fieldset className="form-group">
+        <Field name="passwordConfirm" component={renderField} label="Password Confirm" type="password" />
+      </fieldset>
+      <button type="submit" className="btn btn-primary" disabled={submitting}>Submit</button>          
+    </form>
+  )
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
+})(Signup);
