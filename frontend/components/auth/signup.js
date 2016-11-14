f9e3cc63a6648cc1ea9e50115dc2eb40c8779d0a
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-13 17:43:22 -0800

    implemented signout, implementing signup

---
+++ frontend/components/auth/signup.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+// import Container from './/_container';
+
+class Signup extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div className="">
+        Signup
+      </div>
+    )
+  }
+}
+
+export default Signup;
--- frontend/entry.js
+++ frontend/entry.js
@@ -8,6 +8,7 @@ import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
 import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
 import Signout from './components/auth/signout';
 import reducers from './reducers';
 
@@ -18,7 +19,8 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={App}>
         <Route path="signin" component={Signin} />
-        <Route path="signout" component={Signout} />        
+        <Route path="signout" component={Signout} />
+        <Route path="signup" component={Signup} />
       </Route>
     </Router>
   </Provider>
