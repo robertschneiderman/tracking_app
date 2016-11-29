ae77fb881f02b95d212c5d97751e3c394c263a14
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-28 20:23:49 -0800

    tryig new mongoose config

--- server/index.js
+++ server/index.js
@@ -4,10 +4,22 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const { mongoose } = require('./db/mongoose');
+// const { mongoose } = require('./db/mongoose');
 const router = require('./router');
 
 const app = express();
+
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.Promise = global.Promise;
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    mongoose.connect('mongodb://localhost:auth/auth');
+} else {
+    mongoose.connect('mongodb://heroku_n7jrnqkc:kap0kk3fkirnou5hs767t7h5sv@ds115088-a0.mlab.com:15088,ds115088-a1.mlab.com:15088/heroku_n7jrnqkc?replicaSet=rs-ds115088');
+}
 
 
 // App Setup
