950c187abc0486f62aab6b0526806a4955588bad
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-11-28 19:01:04 -0800

    created production environment variable in node and have node mongo db set

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 // DB Setup
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:auth/auth');
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    mongoose.connect('mongodb://localhost:auth/auth');
+} else {
+    mongoose.connect('mongodb://heroku_n7jrnqkc:kap0kk3fkirnou5hs767t7h5sv@ds115088-a0.mlab.com:15088,ds115088-a1.mlab.com:15088/heroku_n7jrnqkc?replicaSet=rs-ds115088');
+}
 
 module.exports = { mongoose };
