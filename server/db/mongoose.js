9c00402af884306a75375d71c579352b87c6b0ca
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-12-06 20:12:30 -0800

    fixed commented out production db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 var env = process.env.NODE_ENV || 'development';
 
-// if (env === 'development') {
+if (env === 'development') {
     mongoose.connect('mongodb://localhost:auth/auth');
-// } else {
-    // mongoose.connect('mongodb://heroku_n7jrnqkc:kap0kk3fkirnou5hs767t7h5sv@ds115088-a0.mlab.com:15088,ds115088-a1.mlab.com:15088/heroku_n7jrnqkc?replicaSet=rs-ds115088');
-// }
+} else {
+    mongoose.connect('mongodb://heroku_n7jrnqkc:kap0kk3fkirnou5hs767t7h5sv@ds115088-a0.mlab.com:15088,ds115088-a1.mlab.com:15088/heroku_n7jrnqkc?replicaSet=rs-ds115088');
+}
 
 module.exports = { mongoose };
