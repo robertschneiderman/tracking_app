dec651867060c2ab9f6cb02c2c970a4d538257dd
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-12-07 00:51:08 -0800

    fixed extra / problem

--- frontend/util/users_api_util.js
+++ frontend/util/users_api_util.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const ROOT_URL = (process.env.NODE_ENV !== "production") ? 'http://localhost:3090' : 'https://trackyy.herokuapp.com/';
-// const ROOT_URL = 'https://trackyy.herokuapp.com/';
+const ROOT_URL = (process.env.NODE_ENV !== "production") ? 'http://localhost:3090' : 'https://trackyy.herokuapp.com';
+// const ROOT_URL = 'https://trackyy.herokuapp.com';
 
 axios.defaults.headers.common['x-auth'] = localStorage.getItem('token');
 axios.defaults.headers.post['Content-Type'] = 'application/json';
