55db2f62c27065c0f40d3b31d834d85ff063f61d
Robert Schneiderman <Rob@Roberts-MacBook-Pro-3.local>
2016-12-07 00:33:26 -0800

    temporarily using only production v of root url to make it work

--- frontend/actions/authentication/index.js
+++ frontend/actions/authentication/index.js
@@ -6,7 +6,8 @@ import {
   AUTH_ERROR
 } from '../types';
 
-const ROOT_URL = (process.env.NODE_ENV !== "production") ? 'http://localhost:3090' : 'https://trackyy.herokuapp.com/';
+// const ROOT_URL = (process.env.NODE_ENV !== "production") ? 'http://localhost:3090' : 'https://trackyy.herokuapp.com/';
+const ROOT_URL = 'https://trackyy.herokuapp.com/';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
