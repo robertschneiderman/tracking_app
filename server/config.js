cc4c55399ec3c0d8a92689077be41d2e3ca06597
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2016-12-14 12:46:26 -0500

    Delete config.js

--- server/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  secret: 'eP6Xvn6gqVjv0u0BQwgpplpd',
-  sgApiKey: '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'
-};
-  // malgunUser: 'postmaster@tracky.robertschneiderman.com',
-  // mailgunPass: '54fadbb75abe5f34ecb916bfbe16af23',
